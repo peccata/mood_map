2bf17d141efc85f5e5930147b0681395efd536a8
Elena Filatova <peccata@eleison.tk>
2013-10-13 23:58:57 +0700

    Identify moods by timestamp

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -3,4 +3,16 @@ class Mood < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :state, presence: true, inclusion: { in: 1..10 }
   validates :user_id, presence: true
+
+  def new_enough?
+    ((Time.now - created_at) < 1.hour)
+  end
+
+  def timestamp
+    created_at.to_i
+  end
+
+  def to_param
+    timestamp
+  end
 end
